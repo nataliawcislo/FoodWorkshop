8bc130cd3a0043506be6dcd606cd325f61673d67
Natalia Wcisło <wcislonatalia1998@gmail.com>
2020-10-08 16:52:23 +0200

    Initial Commit

---
+++ FoodWorkshop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodWorkshop
+//
+//  Created by Natalia Wcisło on 08/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodWorkshop/FoodWorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodWorkshopApp.swift
+//  FoodWorkshop
+//
+//  Created by Natalia Wcisło on 08/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct FoodWorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
