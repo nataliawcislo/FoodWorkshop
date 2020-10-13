08785ab004f65de113624511e910da096fc8e1e8
Natalia Wcisło <wcislonatalia1998@gmail.com>
2020-10-13 17:38:25 +0200

    feat: add Podfile

---
+++ FoodWorkshop/ArticleView.swift
@@ -0,0 +1,20 @@
+//
+//  ArticleView.swift
+//  FoodWorkshop
+//
+//  Created by Natalia Wcisło on 13/10/2020.
+//
+
+import SwiftUI
+
+struct ArticleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArticleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArticleView()
+    }
+}
--- FoodWorkshop/GridView.swift
+++ FoodWorkshop/GridView.swift
@@ -8,48 +8,51 @@
 import SwiftUI
 
 struct GridView: View {
-    @State private var showDetails: Producer? = nil
-    let columns = 2
-    let items: [Producer]
-    
     var body: some View {
-        var grid: [[Int]] = []
-        
-        _ = (0..<items.count).publisher
-            .collect(columns)
-            .collect()
-            .sink(receiveValue: { grid = $0 })
-        
-        return ForEach(0..<grid.count, id: \.self) { collect in
-            HStack {
-                ForEach(grid[collect], id: \.self) { number in
-                    ZStack {
-                        Button(action: {
-                            withAnimation {
-                                self.showDetails = self.showDetails == nil ? self.items[number] : nil
-                            }
-                        }) {
-                            Image(self.items[number].filename)
-                            .renderingMode(.original)
-                            .resizable()
-                            .aspectRatio(1.0, contentMode: .fit)
-                            .cornerRadius(30)
-                        }
-                        .buttonStyle(PlainButtonStyle())
-                        
-                        
-                    }.overlay(ZStack {
-                        if self.showDetails?.id == self.items[number].id {
-                            Image(self.items[number].filename)
-                                .resizable()
-                                .scaledToFit()
-                                .edgesIgnoringSafeArea(.all)
-                        }
-                    })
-                }
-            }
-        }
+        EmptyView()
     }
+//    @State private var showDetails: Producer? = nil
+//    let columns = 2
+//    let items: [Producer]
+//
+//    var body: some View {
+//        var grid: [[Int]] = []
+//
+//        _ = (0..<items.count).publisher
+//            .collect(columns)
+//            .collect()
+//            .sink(receiveValue: { grid = $0 })
+//
+//        return ForEach(0..<grid.count, id: \.self) { collect in
+//            HStack {
+//                ForEach(grid[collect], id: \.self) { number in
+//                    ZStack {
+//                        Button(action: {
+//                            withAnimation {
+//                                self.showDetails = self.showDetails == nil ? self.items[number] : nil
+//                            }
+//                        }) {
+//                            Image(self.items[number].filename)
+//                            .renderingMode(.original)
+//                            .resizable()
+//                            .aspectRatio(1.0, contentMode: .fit)
+//                            .cornerRadius(30)
+//                        }
+//                        .buttonStyle(PlainButtonStyle())
+//
+//
+//                    }.overlay(ZStack {
+//                        if self.showDetails?.id == self.items[number].id {
+//                            Image(self.items[number].filename)
+//                                .resizable()
+//                                .scaledToFit()
+//                                .edgesIgnoringSafeArea(.all)
+//                        }
+//                    })
+//                }
+//            }
+//        }
+//    }
 }
 
 
